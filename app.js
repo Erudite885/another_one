2f9b6b2f80eb3ec4c3258c26cb463e8128fa4a59
Erudite885 <erudite885@gmail.com>
2023-02-05 10:52:01 +0100

    passing incoming data

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  res.setHeader("Content-Type", "text/html");
-  res.write("<h2>Hi there</h2>");
-  res.end;
+  let body = [];
+  //   console.log(req.method, req.url);
+  req.on("data", (chunk) => {
+    body.push(chunk);
+  });
+  req.on("end", () => {
+    body = Buffer.concat(body).toString();
+    // console.log(body);
+    let userName = "Stranger";
+    if (body) {
+      userName = body.split("=")[1];
+    }
+    res.setHeader("Content-Type", "text/html");
+    res.write(
+      `<h1>Hi ${userName}</h1><form method='post' action='/'><input name='username' type='text'><button type='submit'>Send</button></input></form>`
+    );
+    res.end();
+  });
 });
 
 server.listen(3000);
